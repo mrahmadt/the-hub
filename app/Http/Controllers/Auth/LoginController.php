98a9fef33ba72904f37a69cd1976bdf791e2dd05
AhmadT <ahmadt@gmail.com>
2020-07-22 12:55:27 +0300

    d

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,6 +9,7 @@ use App\User;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 //TODO: do we need to check Azure AD for each action?
 //TODO: How to setup a time out after inactivity
@@ -134,21 +135,32 @@ class LoginController extends Controller
     
     public function teamsToken(Request $request){
 
-        $output = [
+        $json = [
             'status' => 'error',
             'error' => 'Unknown Error',
         ];
 
-        if($request->has('tid')){
-            $tid = $request->input('tid');
+        $post_content = $request->getContent();
+        if($post_content=='') {
+            throw new HttpResponseException(response()->json($json, 422)); 
+        }
+
+        $post_content = \json_decode($post_content);
+        if(json_last_error()!=JSON_ERROR_NONE) exit;
+        if($post_content->tid==''){
+            throw new HttpResponseException(response()->json($json, 422)); 
+        }
+        
+        if(isset($post_content->tid)){
+            $tid = $post_content->tid;
         }else{
             return response()->json([
                 'status' => 'error 1',
                 'error' => 'Unknown Error',
             ],500);
         }
-        if($request->has('token')){
-            $token = $request->input('token');
+        if(isset($post_content->token)){
+            $token = $post_content->token;
         }else{
             return response()->json([
                 'status' => 'error 2',
@@ -159,8 +171,6 @@ class LoginController extends Controller
         $scopes = ["https://graph.microsoft.com/User.Read"];
 
         $url = "https://login.microsoftonline.com/" . $tid . "/oauth2/v2.0/token";
-
-
         $params = [
             'client_id' => config("app.azure_ad_key"),
             'client_secret' => config("app.azure_ad_secret"),
@@ -169,7 +179,6 @@ class LoginController extends Controller
             'requested_token_use' => "on_behalf_of",
             'scope' => implode(' ',$scopes),
         ];
-
         $ch = curl_init();
         curl_setopt_array($ch, [
             CURLOPT_URL => $url,
@@ -183,30 +192,43 @@ class LoginController extends Controller
         ]);
 
         $apiResponse = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch); 
+        $apiResponse_json = \json_decode($apiResponse);
 
-        return response()->json(['code'=>$httpCode,'status'=>'ok','ok'=>true,'error'=>false],200);
-        print_r($httpCode);
-        dd($apiResponse);
+        /*
+
+        {\"error\":\"invalid_grant\"
+            ,\"error_description\":\"AADSTS65001: The user or administrator has not consented to use the application with ID '53cda29e-7798-494b-8cf0-b0b4b50ca52d' named 
+            'The Hub'. Send an interactive authorization request for this user and resource.\\r\\nTrace ID: 87800519-8aec-4ae9-8c4d-50464ea11f00\\r\\nCorrelation ID: ce79db0
+            0-c21a-4037-bde1-d46eb64d595e\\r\\nTimestamp: 2020-07-22 09:49:26Z\",\
+            "error_codes\":[65001],\"timestamp\":\"2020-07-22 09:49:26Z\",\"trace_id\":\"87800519-8aec-4ae9-8c4d-50464ea11f00\",
+            \"correlation_id\":\"ce79db00-c21a-4037-bde1-d46eb64d595e\",\"suberror\":\"consent_required\"}
+
+
+        */
+
+        //print "sdasda";
+        //dd($apiResponse);
+        //return response()->json(['code'=>$httpCode,'status'=>'ok','ok'=>true,'error'=>false],200);
+        //print_r($httpCode);
+        //dd($apiResponse);
         //$jsonArrayResponse - json_decode($apiResponse);
+        curl_close($ch); 
+        return response()->json(['error'=>$apiResponse_json->error],200);
 
 /*
-        var tid = req.body.tid;
-        var token = req.body.token;
-        var scopes = ["https://graph.microsoft.com/User.Read"];
 
-        var oboPromise = new Promise((resolve, reject) => {
-            const url = "https://login.microsoftonline.com/" + tid + "/oauth2/v2.0/token";
-            const params = {
-                client_id: config.get("tab.appId"),
-                client_secret: config.get("tab.appPassword"),
-                grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-                assertion: token,
-                requested_token_use: "on_behalf_of",
-                scope: scopes.join(" ")
-            };
-        
+              if (result.status !== 200) {
+                result.json().then(json => {
+                  // TODO: Check explicitly for invalid_grant or interaction_required
+                  reject({"error":json.error});
+                });
+              } else {
+                result.json().then(json => {
+                  resolve(json.access_token);
+                });
+              }
+
+
             fetch(url, {
               method: "POST",
               body: querystring.stringify(params),
